84dacbdde07b380631cceca686f733a62af2d460
jemily <jemily@jemnem.com>
2013-04-14 10:14:08 -0400

    format changes and library stats

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-
+    @tvshows = Tvshow.all.sort_by(&:ttdb_show_title)
+    @episodes = Episode.where("ttdb_season_number > 0 AND ttdb_episode_airdate < ?", DateTime.now)
+    @completeness = (100 - (@episodes.missing.count.to_f  / @episodes.count.to_f) * 100).round(2)
   end
 end

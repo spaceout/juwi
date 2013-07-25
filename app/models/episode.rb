bb853557c54e60bb2453d1e327535f638f81a5fe
jemily <jemily@jemnem.com>
2013-07-24 20:14:20 -0400

    update missing to NOT include sXXe00 episodes

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,5 +1,5 @@
 class Episode < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :tvshow
-  scope :missing, where("xdb_episode_id IS NULL AND ttdb_season_number IS NOT 0 AND ttdb_episode_airdate <= ?",Date.yesterday)
+  scope :missing, where("xdb_episode_id IS NULL AND ttdb_season_number IS NOT 0 AND ttdb_episode_number IS NOT 0 AND ttdb_episode_airdate <= ?",Date.yesterday)
 end

e61b417ab5f97b14df4eac32f4d71d3e292cb37c
Jeremy Robinson <spaceout@gmail.com>
2014-01-07 14:19:17 -0500

    cant come up with something witty

--- lib/juwi_helper.rb
+++ lib/juwi_helper.rb
@@ -1,5 +1,23 @@
+require 'sequel'
 class JuwiHelper
-  def self.with_xbmcdb
 
+  @@xbmcdb = nil
+  def self.with_xbmcdb
+    @@xbmcdb = get_new_xbmcdb_connection if @@xbmcdb.nil?
+    yield @@xbmcdb
   end
+
+  def self.get_new_xbmcdb_connection
+    Sequel.connect(Setting.get_value("xbmcdb"))
+  end
+
+=begin
+  def self.with_xbmcdb
+    xbmcdb = Sequel.connect(Settings.get_value('xbmcdb'))
+    yield xbmcdb
+    xbmcdb.disconnect
+  end
+=end
+
 end
+

32d2023d5a89749f5072936f1e9588698c935409
jemily <jemily@jarobinsonproosx.urmc-sh.rochester.edu>
2015-06-18 15:58:29 -0400

    clockwork and some delayed

---
+++ app/clock.rb
@@ -0,0 +1,18 @@
+require 'clockwork'
+require './config/boot'
+require './config/environment'
+module Clockwork
+  handler do |job, time|
+    puts "Running #{job}, at #{time}"
+  end
+
+  # handler receives the time when job is prepared to run in the 2nd argument
+  # handler do |job, time|
+  #   puts "Running #{job}, at #{time}"
+  # end
+
+  every(10.seconds,'Poll Transmission') {Torrent.delay(:queue => 'xmission').xmission_check}
+  #every(1.day, :at => '01:00'){Update TTDB}
+  #every(1.day, :at => '02:00'){Update TVR}
+  #every(1.day, :at => '03:00'){do some more shit}
+end
--- app/models/torrent.rb
+++ app/models/torrent.rb
@@ -10,13 +10,65 @@ class Torrent < ActiveRecord::Base
     )
     current_torrents = xmission.all
     current_torrents.each do |torrent|
-      puts "id = #{torrent["id"]}"
-      puts "name = #{torrent["name"]}"
-      puts "percentDone = #{torrent["percentDone"]}"
-      puts "totalSize = #{torrent["totalSize"]}"
-      puts "isFinished = #{torrent["isFinished"]}"
-      puts "downloadDir = #{torrent["downloadDir"]}"
-      puts "hashString = #{torrent["hashString"]}"
+      if torrent["downloadDir"] == Setting.get_value('finished_path')
+        #process_torrent(torrent)
+      end
+    end
+  end
+
+
+  def process_torrent(dl_torrent)
+    db_torrent = Torrent.find_by_hash_string(torrent["hashString"])
+    if db_torrent.nil?
+      if torrent["totalSize"] == 0
+        torrent["name"] = "MAGNET LINK"
+      Torrent.create(
+        :completed => torrent["isDone"],
+        :hash_string => torrent["hashString"],
+        :name => torrent["name"],
+        :percent => torrent["percentDone"],
+        :size => torrent["totalSize"],
+        :status => "Downloading",
+        :time_started => Time.now
+      )
+    else
+      if torrent["isDone"] == false
+        db_torrent.update_attributes(
+          :percent => torrent["percentDone"],
+          :completed => torrent["isDone"],
+        )
+      end
     end
   end
 end
+
+=begin
+    torrent = Torrent.create(hashString)
+    torrent.update_attributes(
+      :completed => torrent["isDone"],
+      :hash_string => torrent["hashString"],
+      :name => torrent[name],
+      :percent => torrent[percentDone],
+      :size => torrent[totalSize],
+      :status => "Downloading",
+      :time_started => Time.now
+    )
+
+    #check download dir is the correct one, ignore other torrents
+      #search for hashString
+      #if not found
+        #New Torrent
+        #insert ID, time_started, Name (if not null, remember about magnet links), PercentDone, totalSize, isfinished, downloadDir and hashString
+      #if found
+        #if isFinished is NOT true
+        #update name if not null
+        #update percentdone and isFinished
+
+puts "id = #{torrent["id"]}"
+puts "name = #{torrent["name"]}"
+puts "percentDone = #{torrent["percentDone"]}"
+puts "totalSize = #{torrent["totalSize"]}"
+puts "isFinished = #{torrent["isFinished"]}"
+puts "downloadDir = #{torrent["downloadDir"]}"
+puts "hashString = #{torrent["hashString"]}"
+=end

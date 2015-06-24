816d619d086dc63a5e6d8899b33e2db5de865971
jemily <jemily@sheldon.jemnem.com>
2015-06-24 06:25:32 -0400

    more torrent tracker

--- app/models/torrent.rb
+++ app/models/torrent.rb
@@ -11,22 +11,31 @@ class Torrent < ActiveRecord::Base
     )
     current_torrents = xmission.all
     current_torrents.each do |torrent|
-      if torrent["downloadDir"] == "#{Setting.get_value('finished_path')}/"
+      if torrent["downloadDir"].chomp("/") == Setting.get_value('finished_path').chomp("/")
         process_torrent(torrent)
       end
     end
   end
 
   def self.process_torrent(dl_torrent)
-    if dl_torrent["totalSize"] == 0
-      dl_torrent["name"] = "MAGNET LINK"
-    end
     db_torrent = Torrent.find_or_initialize_by_hash_string(dl_torrent["hashString"])
+    dl_torrent_size = dl_torrent["totalSize"]
+    if dl_torrent_size == 0
+      dl_torrent["name"] = "MAGNET LINK"
+      db_torrent.update_attributes(:status => "Downloading Metadata")
+    end
+    if [nil,0].include?(db_torrent.size)
+      if dl_torrent["totalSize"] != 0
+        db_torrent.update_attributes(
+        :status => "Downloading"
+        )
+      end
+    end
     if dl_torrent["isFinished"]
       if !db_torrent.completed
         db_torrent.update_attributes(
-        :time_completed => Time.now,
-        :status => "Completed"
+        :time_completed => DateTime.now,
+        :status => "Download Completed"
         )
       end
     end
@@ -34,10 +43,10 @@ class Torrent < ActiveRecord::Base
       :completed => dl_torrent["isFinished"],
       :hash_string => dl_torrent["hashString"],
       :name => dl_torrent["name"],
-      :percent => dl_torrent["percentDone"] * 100,
+      :percent => (dl_torrent["percentDone"] * 100),
       :size => dl_torrent["totalSize"],
-      :time_started => dl_torrent["addedDate"],
-      :files => dl_torrent["files"]
+      :files => dl_torrent["files"],
+      :time_started => Time.at(dl_torrent["addedDate"]).utc.to_datetime,
     )
   end
 end
